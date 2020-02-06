02242ece0ca9e31492a278578a0c7777669aa8dc
Valerio Bozzolan <boz@reyboz.it>
2020-02-06 07:05:10 +0000

    show some Podcasts in the homepage
    Closes T166

--- index.php
+++ index.php
@@ -63,6 +63,57 @@ Author URI: http://qantumthemes.com
                     </div>
                 </div>
             </div>
+
+
+		<?php
+		/**
+		 * Homepage redesign
+		 *
+		 * This block was imported from:
+		 *  /wp-content/themes/onair2/archive-podcast.php
+		 *
+		 * See https://gitpull.it/T166
+		 */
+		?>
+		<!-- start podcasts -->
+		<div class="qt-container qt-vertical-padding-l  qt-archive-team">
+			<div class="row">
+				<?php
+				/**
+				 * [$args Query arguments]
+				 * @var array
+				 */
+				$args = array(
+					'post_type' => 'podcast',
+					'posts_per_page' => 9,
+					'post_status' => 'publish',
+					'suppress_filters' => false,
+					'orderby' => 'meta_value',
+					'order'   => 'DESC',
+					'meta_key' => '_podcast_date',
+					'paged' => $paged
+				);
+				/**
+				 * [$wp_query execution of the query]
+				 * @var WP_Query
+				 */
+				$wp_query = new WP_Query( $args );
+				if ( $wp_query->have_posts() ) : while ( $wp_query->have_posts() ) : $wp_query->the_post();
+					?>
+					<div id="post-<?php the_ID(); ?>" <?php post_class( "col s12 m6 l4" ); ?>>
+						<?php get_template_part (  'phpincludes/part-archive-item-podcast' ); ?>
+					</div>
+					<?php
+				endwhile; else: ?>
+					<h3><?php echo esc_attr__("Sorry, nothing here","onair2")?></h3>
+				<?php endif;
+
+				wp_reset_postdata();
+				?>
+			</div>
+		</div>
+		<!-- /end podcasts -->
+
         </div>
     </div><!-- .qt-main end -->
     <?php get_template_part ( 'phpincludes/part-sponsors' ); ?>
