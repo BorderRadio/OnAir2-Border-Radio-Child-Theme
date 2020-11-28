aa24f8fd8753312f8b0959f07a054ccd6377b8f3
Alessandro Lepori <leporialessandro18@gmail.com>
2020-11-28 13:36:35 +0000

    Homepage: put podcasts above news
    Closes T582

--- index.php
+++ index.php
@@ -24,7 +24,46 @@ Author URI: http://qantumthemes.com
             <!-- ======================= HEADER SECTION ======================= -->
             <?php get_template_part( 'phpincludes/part-header-caption-archive'); ?>
             <!-- ======================= CONTENT SECTION ======================= -->
-            <div class="qt-container qt-vertical-padding-l ">
+            <!-- start podcasts -->
+		<div class="qt-container qt-vertical-padding-l  qt-archive-team">
+			<h3>Ultimi Podcast</h3>
+			<div class="row">
+				<?php
+				/**
+				 * [$args Query arguments]
+				 * @var array
+				 */
+				$args = array(
+					'post_type' => 'podcast',
+					'posts_per_page' => 9,
+					'post_status' => 'publish',
+					'suppress_filters' => false,
+					'orderby' => 'meta_value',
+					'order'   => 'DESC',
+					'meta_key' => '_podcast_date',
+					'paged' => $paged
+				);
+				/**
+				 * [$wp_query execution of the query]
+				 * @var WP_Query
+				 */
+				$wp_query = new WP_Query( $args );
+				if ( $wp_query->have_posts() ) : while ( $wp_query->have_posts() ) : $wp_query->the_post();
+					?>
+					<div id="post-<?php the_ID(); ?>" <?php post_class( "col s12 m6 l4" ); ?>>
+						<?php get_template_part (  'phpincludes/part-archive-item-podcast' ); ?>
+					</div>
+					<?php
+				endwhile; else: ?>
+					<h3><?php echo esc_attr__("Sorry, nothing here","onair2")?></h3>
+				<?php endif;
+
+				wp_reset_postdata();
+				?>
+			</div>
+		</div>
+		<!-- /end podcasts -->
+	    <div class="qt-container qt-vertical-padding-l ">
 		<div class="row">
                         <?php
                             /**
@@ -72,45 +111,7 @@ Author URI: http://qantumthemes.com
 		 * See https://gitpull.it/T166
 		 */
 		?>
-		<!-- start podcasts -->
-		<div class="qt-container qt-vertical-padding-l  qt-archive-team">
-			<h3>Ultimi Podcast</h3>
-			<div class="row">
-				<?php
-				/**
-				 * [$args Query arguments]
-				 * @var array
-				 */
-				$args = array(
-					'post_type' => 'podcast',
-					'posts_per_page' => 9,
-					'post_status' => 'publish',
-					'suppress_filters' => false,
-					'orderby' => 'meta_value',
-					'order'   => 'DESC',
-					'meta_key' => '_podcast_date',
-					'paged' => $paged
-				);
-				/**
-				 * [$wp_query execution of the query]
-				 * @var WP_Query
-				 */
-				$wp_query = new WP_Query( $args );
-				if ( $wp_query->have_posts() ) : while ( $wp_query->have_posts() ) : $wp_query->the_post();
-					?>
-					<div id="post-<?php the_ID(); ?>" <?php post_class( "col s12 m6 l4" ); ?>>
-						<?php get_template_part (  'phpincludes/part-archive-item-podcast' ); ?>
-					</div>
-					<?php
-				endwhile; else: ?>
-					<h3><?php echo esc_attr__("Sorry, nothing here","onair2")?></h3>
-				<?php endif;
-
-				wp_reset_postdata();
-				?>
-			</div>
-		</div>
-		<!-- /end podcasts -->
+		
 
         </div>
     </div><!-- .qt-main end -->
